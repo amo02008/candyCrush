72515b7514475244592bb55d090e0949e6816527
amo02008 <adrienne.osorio@gmail.com>
2021-08-04 15:14:52 -0400

    board created with random colors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
    const grid = document.querySelector('.grid')
    const width = 8
    const squares = []
@@ -9,12 +9,15 @@ document.addEventListener('DOMContentLoaded', () =>{
        'orange',
        'purple',
        'green',
+       'blue',
    ]
 
    //Create Board
    function createBoard() {
        for (let i=0; i < width*width; i++) {
            const square = document.createElement('div')
+           let randomColor = Math.floor(Math.random() * candyColors.length)
+           square.style.backgroundColor = candyColors [randomColor]
            grid.appendChild(square)
            squares.push(square)
        }
